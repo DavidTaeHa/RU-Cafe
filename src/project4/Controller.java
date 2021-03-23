57c95698d56015d8f460b333014709cdd50fccd3
Gandhi115 <61490541+Gandhi115@users.noreply.github.com>
2021-03-23 18:41:50 -0400

    Add classes

--- src/project4/Controller.java
+++ src/project4/Controller.java
@@ -1,4 +1,4 @@
-package sample;
+package project4;
 
 public class Controller {
 }
--- src/project4/Main.java
+++ src/project4/Main.java
@@ -1,4 +1,4 @@
-package sample;
+package project4;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
