665405af92f1d4040e53ce80c355d0d59f99f496
Gandhi115 <61490541+Gandhi115@users.noreply.github.com>
2021-03-23 19:04:09 -0400

    Add Javadoc Comments

--- src/project4/Donut.java
+++ src/project4/Donut.java
@@ -2,9 +2,9 @@ package project4;
 
 /**
  * This class extends the MenuItem class and contains common attributes and operations specific to an
- * instance of a coffee
+ * instance of a donut
  *
  * @author David Ha, Andrew McAvoy
  */
-public class Coffee extends MenuItem {
+public class Donut {
 }
