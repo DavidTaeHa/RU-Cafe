216ad2c2bc410dee0a682467c027223a36475568
Gandhi115 <61490541+Gandhi115@users.noreply.github.com>
2021-03-23 20:17:20 -0400

    Update Coffee.java

--- src/project4/Coffee.java
+++ src/project4/Coffee.java
@@ -6,5 +6,120 @@ package project4;
  *
  * @author David Ha, Andrew McAvoy
  */
-public class Coffee extends MenuItem {
+public class Coffee extends MenuItem implements Customizable {
+    private int size;
+    private boolean[] addIn;
+    private int addInQuantity;
+
+    final static int SHORT = 1;
+    final static int TALL = 2;
+    final static int GRANDE = 3;
+    final static int VENTI = 4;
+
+    final static double SHORT_PRICE = 1.99;
+    final static double TALL_PRICE = 2.49;
+    final static double GRANDE_PRICE = 2.99;
+    final static double VENTI_PRICE = 3.49;
+    final static double ADDIN_PRICE = 0.20;
+
+    final static int CREAM = 0;
+    final static int SYRUP = 1;
+    final static int MILK = 2;
+    final static int CARAMEL = 3;
+    final static int WHIPPED_CREAM = 4;
+
+    final static int NUM_OF_ADDINS = 5;
+    final static int INVALID = -1;
+
+    public Coffee(String name, int size) {
+        super(name);
+        this.size = size;
+        this.addIn = new boolean[NUM_OF_ADDINS];
+        this.addInQuantity = 0;
+    }
+
+    /**
+     * Calculates the price of an instance of a coffee
+     */
+    @Override
+    public void calculateItemPrice() {
+        switch (size) {
+            case SHORT:
+                super.setItemPrice(SHORT_PRICE + (addInQuantity * ADDIN_PRICE));
+                break;
+            case TALL:
+                super.setItemPrice(TALL_PRICE + (addInQuantity * ADDIN_PRICE));
+                break;
+            case GRANDE:
+                super.setItemPrice(GRANDE_PRICE + (addInQuantity * ADDIN_PRICE));
+                break;
+            case VENTI:
+                super.setItemPrice(VENTI_PRICE + (addInQuantity * ADDIN_PRICE));
+        }
+    }
+
+    /**
+     * Adds a coffee add-in to the addIn container
+     *
+     * @param obj addIn to be added to the coffee
+     * @return true if successfully added; false if otherwise
+     */
+    @Override
+    public boolean add(Object obj) {
+        int index;
+        if (obj instanceof String) {
+            index = getIndex((String) obj);
+            if ((index > INVALID) && (addIn[index] != true)) {
+                addIn[index] = true;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Removes a coffee add-In from the addIn container
+     *
+     * @param obj addIn to be removed from the coffee
+     * @return true of successfully removed; false if otherwise
+     */
+    @Override
+    public boolean remove(Object obj) {
+        int index;
+        if (obj instanceof String) {
+            index = getIndex((String) obj);
+            if ((index > INVALID) && (addIn[index] != false)) {
+                addIn[index] = false;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Helper method to aid in retrieval of index of the add-in component relative to the addIn container
+     *
+     * @param obj Add-in to be added to the coffee
+     * @return index of the add-in in the addIn container
+     */
+    private int getIndex(String obj) {
+        int index = INVALID;
+        switch (obj) {
+            case "Cream":
+                index = CREAM;
+                break;
+            case "Syrup":
+                index = SYRUP;
+                break;
+            case "Milk":
+                index = MILK;
+                break;
+            case "Caramel":
+                index = CARAMEL;
+                break;
+            case "Whipped Cream":
+                index = WHIPPED_CREAM;
+        }
+        return index;
+    }
 }
--- src/project4/MenuItem.java
+++ src/project4/MenuItem.java
@@ -6,4 +6,43 @@ package project4;
  * @author David Ha, Andrew McAvoy
  */
 public class MenuItem {
+    private String name;
+    private int quantity;
+    private double itemPrice;
+
+    /**
+     * Constructor for MenuItem class
+     *
+     * @param name Name of the item
+     */
+    public MenuItem(String name){
+        this.name = name;
+        this.quantity = 0;
+        this.itemPrice = 0;
+    }
+
+    /**
+     * Setter method for item price
+     *
+     * @param itemPrice Price of an item
+     */
+    public void setItemPrice(double itemPrice) {
+        this.itemPrice = itemPrice;
+    }
+
+    /**
+     * Setter method for quantity
+     *
+     * @param quantity Number of an item
+     */
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    /**
+     * Method for the subclasses to implement and override for calculating item price
+     */
+    public void calculateItemPrice(){
+        //Overriden by subclasses
+    }
 }
