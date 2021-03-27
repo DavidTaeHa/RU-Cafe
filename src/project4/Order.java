7b89c818f9b0c567b09e6aae70efefa0283a340c
Gandhi115 <61490541+Gandhi115@users.noreply.github.com>
2021-03-27 17:51:29 -0400

    Update Order.java

--- src/project4/Order.java
+++ src/project4/Order.java
@@ -7,8 +7,11 @@ import javafx.collections.ObservableList;
  *
  * @author David Ha, Andrew McAvoy
  */
-public class Order {
+public class Order implements Customizable{
     private ObservableList<MenuItem> items;
+    private double subtotal;
+    private double taxTotal;
+    private double total;
     private static int orderNumber = 1;
 
     final static double TAX = 0.06625;
@@ -18,5 +21,60 @@ public class Order {
      */
     public Order(){
         this.items = FXCollections.observableArrayList();
+        subtotal = 0;
+    }
+
+    /**
+     * Setter method for order number
+     *
+     * @param orderNumber number to be incremented
+     */
+    public static void setOrderNumber(int orderNumber) {
+        Order.orderNumber = orderNumber;
+    }
+
+    /**
+     * Adds items from the menu to the order container and adds to subtotal
+     *
+     * @param obj menu item to be added to the order
+     * @return
+     */
+    @Override
+    public boolean add(Object obj) {
+        if(obj instanceof MenuItem){
+            if(!items.contains(obj)){
+                items.add((MenuItem)obj);
+                subtotal = subtotal + ((MenuItem) obj).getItemPrice();
+                taxTotal = subtotal * TAX;
+                total = subtotal + taxTotal;
+            }
+            else if (items.contains(obj)){
+                MenuItem temp = items.get(items.indexOf(obj));
+                temp.setQuantity(((MenuItem) obj).getQuantity() + temp.getQuantity());
+                subtotal = subtotal + ((MenuItem) obj).getItemPrice();
+                taxTotal = subtotal * TAX;
+                total = subtotal + taxTotal;
+            }
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Removes items from the order container
+     *
+     * @param obj item to be removed from order
+     * @return
+     */
+    @Override
+    public boolean remove(Object obj) {
+        if(obj instanceof Donut){
+            items.remove(obj);
+            subtotal = subtotal - ((MenuItem) obj).getItemPrice();
+            taxTotal = subtotal * TAX;
+            total = subtotal + taxTotal;
+            return true;
+        }
+        return false;
     }
 }
